7d8e5ded753bf8c90bf1ccc02eba492491034f13
Fabian Rauch <mail@rauch-fabian.de>
2023-05-02 12:26:44 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Random Student and Expression</h1>
+    <button @click="pickRandomCombination" :disabled="allExpressionsUsed">
+      Pick Random Combination
+    </button>
+    <p v-if="allExpressionsUsed">All expressions have been used</p>
+    <p class="result">{{ randomCombination }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import students from "@/_examples/students.json";
+import expressions from "@/_examples/expressions.json";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      usedExpressions: [],
+      randomCombination: "",
+    };
+  },
+  computed: {
+    allExpressionsUsed() {
+      return this.usedExpressions.length === expressions.length;
+    },
+  },
+  methods: {
+    pickRandomCombination() {
+      const unusedExpressions = expressions.filter(
+        (expression) => !this.usedExpressions.includes(expression.expression)
+      );
+      if (unusedExpressions.length === 0) {
+        this.randomCombination = "All expressions have been used";
+        return;
+      }
+      const randomStudent =
+        students[Math.floor(Math.random() * students.length)];
+      const randomExpression =
+        unusedExpressions[Math.floor(Math.random() * unusedExpressions.length)];
+      this.usedExpressions.push(randomExpression.expression);
+      this.randomCombination = `${randomStudent.firstName} ${randomStudent.lastName} - ${randomExpression.expression}`;
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.result {
+  font-size: 2em;
 }
 </style>
